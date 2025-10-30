049045dd5d6ad0ca497f42504e19c1877405ed98
mohidHussain <mohidhussaindot@gmail.com>
2025-10-30 22:56:40 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module} from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -10,16 +10,19 @@ import { NationalitiesModule } from './nationalities/nationalities.module';
 
 @Module({
   imports: [
- ConfigModule.forRoot({ isGlobal: true }),
+  // Load .env globally (MUST come before TypeOrmModule)
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env', // ensures it reads from your local .env
+    }),
 
+    // Database config
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      ssl: { rejectUnauthorized: false },
     }),
     AuthModule,
     CountriesModule,
